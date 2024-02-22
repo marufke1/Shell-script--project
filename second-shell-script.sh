c716ede8c4b1e57e6cda78810154cc01e657c641
root <root@ip-172-31-1-77.ap-south-1.compute.internal>
2024-02-22 03:29:25 +0000

    2nd shell-script commit

---
+++ second-shell-script.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+
+
+##### Using metadata that provides information about the script we are using it
+
+
+# Author: Maruf Hasan
+# Date: 2024-02-21
+# Description: By using this script going to create a folder and file in the home directory
+# Version: V1
+
+##### create a folder
+
+mkdir Devops
+
+##### change the directory
+
+cd Devops/
+
+
+##### finally create a file inside of the directory
+
+touch file1.txt
+
+echo "I already learnt how to create a file by using shell script now" > file2.txt
+
+
+
