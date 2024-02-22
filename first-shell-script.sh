92b8da122e7462f04f53e1f947c0dbfa133c6e5a
root <root@ip-172-31-1-77.ap-south-1.compute.internal>
2024-02-22 03:10:44 +0000

    1st shell-script commit

---
+++ first-shell-script.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+
+#### This is my 1st shell script for leaning devops 
+
+
+# Author: Maruf Hasan
+# Date: 2024-02-21
+# Description: Using echo command to printout something on the terminal
+# Version: v1
+
+echo "I am learning shell scripting to automate tasks for Devops "
+
+
