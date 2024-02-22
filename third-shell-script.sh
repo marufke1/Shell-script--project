5d66ac23e86eae2bb1544f5b4fcd48b276be7628
root <root@ip-172-31-1-77.ap-south-1.compute.internal>
2024-02-22 03:43:51 +0000

    3rd shell-script commit

---
+++ third-shell-script.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+
+#### using metdata that provides the information about the script we are using it
+
+
+# Author: Maruf Hasan
+# Date: 2024-02-21
+# Description: to perform health check for the nodes
+# Version: V1
+
+set = X ### for debugging the code
+
+df -h ## show the output of the storage
+
+free -m ## show the  output of the memory
+
+nproc ## show the output of the cpu capacity
+
+
+
