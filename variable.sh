f1112f08a4330886cd926ff001bba271b6d6bdca
root <root@ip-172-31-4-227.ap-south-1.compute.internal>
2024-02-23 03:55:51 +0000

    5th shell-script commit

---
+++ variable.sh
@@ -0,0 +1,19 @@
+
+
+#!/bin/bash
+
+# script to show how to use variables
+
+
+# Author: Maruf Hasan
+# Date: 2024-02-22
+# Description: Create a variable by using different values like - string, int etc..
+# Version: V1
+
+name="Maruf"
+age=35
+gender="male"
+
+echo "My name is $name, I am $age years old and gender is $gender"
+
+
