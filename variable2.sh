1c48eb29e264bac6d2b13f820e863c499453d784
root <root@ip-172-31-4-227.ap-south-1.compute.internal>
2024-02-27 02:18:07 +0000

    other-variable commit

---
+++ variable2.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+
+# Variable also can store any linux command outputs.
+
+# Author: Maruf Hasan
+# Date: 2024-02-26
+# Description: store the out put of hostname command into a variable
+# Version: V1
+
+hostname=$(hostname)
+echo "my machine name is $hostname"
+
+
