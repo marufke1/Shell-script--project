1db06f0db3335f0920549149ba446dce29cd106d
root <root@ip-172-31-4-227.ap-south-1.compute.internal>
2024-02-27 01:34:57 +0000

    comment commit

---
+++ comment.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+echo "Comment is very important concept for shell scripting that allows to provide description
+about the script but anything mention in the comment will not be executed when run the script."
+
+# This is my single line comment
+
+<< comment
+
+This is
+my
+multi line
+comments
+
+comment
+
+
