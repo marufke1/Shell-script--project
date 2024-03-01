1c0d26dbb2954e77451f604fcfd1ba9de19ba1c9
root <root@ip-172-31-4-227.ap-south-1.compute.internal>
2024-03-01 02:43:24 +0000

    user-interact commit

---
+++ user-interact.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+
+<< comment
+
+user interaction is going to allow to get the
+input from the users!!
+
+comment
+
+# Author: Maruf Hasan
+# Date: 2024-02-29
+# Description: user-interaction
+# Version: V1
+
+echo "What is your name?"
+read name
+echo "your name is $name"
+
+echo "How old are you?"
+read age
+echo "your age is $age"
+
+echo "What is your gender?"
+read gender
+echo "your gender is $gender"
+
+echo "What is your address?"
+read address
+echo "your address is $address"
