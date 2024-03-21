9e9f445451976e660bd9e27369dc105ed5041d36
root <root@ip-172-31-4-227.ap-south-1.compute.internal>
2024-03-21 02:01:50 +0000

    argument commit

---
+++ argument.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+
+<< comment
+Argument are values that are passed
+to a script!!
+comment
+
+# Author: Maruf Hasan
+# Date: 2024-03-20
+# Description: using argument to pass multiple values in the script
+# Version: V1
+
+echo "the content of the files are $0 $1 $2"
+
+
