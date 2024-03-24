bad1696de8e5613ced2a1761684ae72dd47d03a7
root <root@ip-172-31-4-227.ap-south-1.compute.internal>
2024-03-24 17:00:46 +0000

    while-loop commit

---
+++ whileloop.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+<< comment
+while loop allows to execute set od commands repeatidely
+until the condition true.
+comment
+
+# Author: Maruf Hasan
+# Date: 24-03-2024
+# Description: using condition to run while loop until the condition is true
+# version: V1
+
+
+num=0
+
+while [[ $num -le 5 ]]
+
+do
+	echo "This is the number"
+	 num=$num+1
+done
